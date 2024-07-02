b9488a0e9a511ef9d13139618ded7f2aad2c57d3
malt <maltvinegarandchips@gmail.com>
2024-07-02 16:56:13 -0400

    More changes

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,16 @@
 #include <ncurses.h>
 #include <panel.h>
 
-char scrn[18][58] { "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+char scrn[48][96] { "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"};
+
+//213x60
+//60 93 60
+//213x20
 
 int main() {
 	for(auto &i : scrn)
 		strcpy(i, scrn[0]);
+
 	initscr();
 	noecho();
 	cbreak();
@@ -19,66 +24,70 @@ int main() {
 	keypad(stdscr, TRUE);
 
 	int ch, done;
-	WINDOW *win1, *win2, *focus;
+	WINDOW *stats, *playarea, *chat, *inventory;
 
-	win1 = newwin(20, 60, 0, 0);
-	win2 = newwin(20, 30, 0, 60);
-	wrefresh(win1);
-	wrefresh(win2);
 
-	focus = win1;
+	stats = newwin(50, 57, 0, 0);
+	playarea = newwin(50, 98, 0, 57);
+	chat = newwin(50, 57, 0, 155);
+	inventory = newwin(10, 212, 50, 0);
 
-	keypad(win1, TRUE);
-	keypad(win2, TRUE);
+	wrefresh(stats);
+	wrefresh(playarea);
+	wrefresh(chat);
+	wrefresh(inventory);
+
+	keypad(stats, TRUE);
+	keypad(playarea, TRUE);
+	keypad(chat, TRUE);
+	keypad(inventory, TRUE);
+
 	mousemask(ALL_MOUSE_EVENTS, NULL);
 
-        MEVENT event;
+	MEVENT event;
 	done = 0;
 
 	while(!done) {
-		box(win1, 0, 0);
-		wattron(win2, A_STANDOUT);
-		box(win2, 0, 0);
-		wattroff(win2, A_STANDOUT);
+		box(stats, 0, 0);
+		box(playarea, 0, 0);
+		box(chat, 0, 0);
+		box(inventory, 0, 0);
 		ch = getch();
 
 		if(ch == 'q')
 			done = 1;
-		else if(ch == 'a')
-			focus = win2;
-		else if(ch == 'p') {
-			for(unsigned i {0}; i < 58; i++)
-				mvwprintw(win1, i+1, 1, "%s", scrn[i]);
-		} else if(ch == KEY_MOUSE) {
+		else if(ch == 'p')
+			for(unsigned i {0}; i < 48; i++)
+				mvwprintw(playarea, i+1, 1, "%s", scrn[i]);
+		else if(ch == KEY_MOUSE) {
 			if(getmouse(&event) == OK) {
 				if(event.bstate & (BUTTON1_CLICKED|BUTTON1_PRESSED|BUTTON1_DOUBLE_CLICKED|BUTTON1_TRIPLE_CLICKED|BUTTON1_RELEASED)) {
-					mvwprintw(win1, 2, 1, "You pressed the button1");
-					if(event.x < 60 && event.y < 20)
-						focus = win1;
-					else if(event.x < 90 && event.y < 20)
-						focus = win2;
+					mvwprintw(playarea, 2, 1, "You pressed the button1");
 				} else if(event.bstate & BUTTON2_PRESSED)
-					mvwprintw(win1, 2, 1, "You pressed the button2");
+					mvwprintw(playarea, 2, 1, "You pressed the button2");
 				else
-					mvwprintw(win1, 2, 1, "You pressed the key %i", event.bstate);
-				mvwprintw(win1, 3, 1, "(x %i, y %i)", event.x, event.y);
-				mvwprintw(win1, event.y, event.x, "(x %i, y %i)", event.x, event.y);
+					mvwprintw(playarea, 2, 1, "You pressed the key %i", event.bstate);
+				mvwprintw(playarea, 3, 1, "(x %i, y %i)", event.x, event.y);
+				mvwprintw(stdscr, event.y, event.x, "(x %i, y %i)", event.x, event.y);
 			}
 		} else
-			mvwprintw(win1, 2, 1, "You pressed the key %c", ch);
+			mvwprintw(playarea, 2, 1, "You pressed the key %c", ch);
 
-		wattron(focus, A_STANDOUT);
-		mvwprintw(focus, 1, 1, "Focus is on this window");
-		wattroff(focus, A_STANDOUT);
 
-		wrefresh(win1);
-		wrefresh(win2);
-		wclear(win1);
-		wclear(win2);
+		wrefresh(stats);
+		wrefresh(playarea);
+		wrefresh(chat);
+		wrefresh(inventory);
+		wclear(stats);
+		wclear(playarea);
+		wclear(chat);
+		wclear(inventory);
 	}
 
-	delwin(win1);
-	delwin(win2);
+	delwin(stats);
+	delwin(playarea);
+	delwin(chat);
+	delwin(inventory);
 	erase();
 
 	endwin();
